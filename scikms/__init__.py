b5fcd2ef861bc67549e1ec106304b7f6edacdb73
CocDap <hodinhdung1997@gmail.com>
2026-04-22 23:41:25 +0700

    chore: fix version

--- scikms/__init__.py
+++ scikms/__init__.py
@@ -4,4 +4,8 @@ Top-level package. Import site effects are kept minimal so headless tests and
 tooling can ``import scikms`` without spinning up any Qt or filesystem state.
 """
 
-__version__ = "0.1.0"
+from importlib.metadata import version as _v, PackageNotFoundError as _E
+try:
+    __version__ = _v("scikms")
+except _E:
+    __version__ = "0.1.0"
